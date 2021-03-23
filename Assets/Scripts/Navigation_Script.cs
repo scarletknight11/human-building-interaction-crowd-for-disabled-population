529e5d0325c7e2e5714764cb50699d7a2eedcb9e
kinjalp516 <kinjalp516@gmail.com>
2021-03-23 01:03:14 -0600

    Added multiple agents who stop walking at destination

---
+++ Assets/Scripts/Navigation_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation_Script : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>(); 
+        animator = this.GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish")) {
+            animator.runtimeAnimatorController = Resources.Load("Kevin Iglesias/Basic Motions Pack/AnimationControllers/BasicMotions@Idle.controller") as RuntimeAnimatorController;
+        }
+    }
+}
