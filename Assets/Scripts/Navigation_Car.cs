8433576b857da74b05cc9a10503803fae0b91c12
kinjalp516 <kinjalp516@gmail.com>
2021-04-20 15:13:27 -0600

    Added wheelchair rotation for more realistic movement

---
+++ Assets/Scripts/Navigation_Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation_Car : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt (new Vector3(target.position.x, transform.position.y, target.position.z));
+        agent.SetDestination(target.position);
+    }
+}
