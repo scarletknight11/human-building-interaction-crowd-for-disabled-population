4d094a9ce54fd6bd7eca533870a1ab8845e6a9f7
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-04-21 01:25:37 -0400

    modifed evacuation for intelligent agents

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int count;
+    public int Evaccountcount;
+    public Text EvaccountText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        Evaccountcount = 0;
+        EvaccountText.text = "Evacuation: ";
+    }
+
+ 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.tag == "Player")
+        {
+            count = count + 1;
+            CollisionCountText();
+        }
+    }
+
+    void CollisionCountText()
+    {
+        Evaccountcount = Evaccountcount + 1;
+        EvaccountText.text = "Evacuation: " + Evaccountcount.ToString();
+    }
+}
--- Assets/Scripts/Navigation_Script.cs
+++ Assets/Scripts/Navigation_Script.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.AI;
 
 public class Navigation_Script : MonoBehaviour
 {
     public Transform target;
+    public GameObject agentevac;
     NavMeshAgent agent;
     Animator animator;
+
+ 
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>(); 
-        animator = this.GetComponent<Animator>();  
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,8 +28,14 @@ public class Navigation_Script : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Finish")) {
+        if (other.CompareTag("Finish"))
+        {
             animator.runtimeAnimatorController = Resources.Load("Kevin Iglesias/Basic Motions Pack/AnimationControllers/BasicMotions@Idle.controller") as RuntimeAnimatorController;
+            agentevac.SetActive(false);
+            //count = count + 1;
+            //EvaccountText.text = "Evacuation: " + count.ToString();
         }
-    }
+    } 
+    
+
 }
